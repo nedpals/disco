2900a6a60531f4e881570ecdba08ff55e3e2766e
Ned Palacios <npdoesmc@gmail.com>
2019-06-25 18:57:51 +0800

    Update cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,25 @@
 import { CLI } from "kuman";
-import runCommand from "./commands/run"
+import runCommand from "./commands/run";
+import checkCommand from "./commands/check";
+import { getAppInfo } from "./utils";
 
 const cli = CLI();
 const app = {
     name: "Disco",
     description: "Create Discord bots fast.",
-    version: "1.0.0" 
+    version: getAppInfo("version")
 };
 
 cli.on("showVersion", () => console.log(app.version));
-
 cli.on("showHelp", () => console.log(`${app.name}\n${app.description}`));
-
-cli.command("init", () => {
-    console.log("Init");
-}, {
-    arguments: 1,
-    description: "Initializes a new Disco project."
-});
 
 cli.command("run", runCommand, {
     arguments: 1,
     description: "Runs the Discord bot."
+});
+
+cli.command("check", checkCommand, {
+    description: "Prints Disco version and system info. Also checks .env file."
 });
 
 module.exports = cli;
